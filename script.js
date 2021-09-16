884cd54350b576221a9bc11db90e500043101627
YanLivarenko <laotzu211@gmail.com>
2021-09-17 01:36:07 +0300

    fix deleteItem

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// Необходимо сделать:
-// Удаление элементов
-// Сохранение данных в браузере
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const todoInput = document.querySelector('#todoInput');
     const todoList = document.querySelector('#todoList');
     const todoButton = document.querySelector('#todoButton');
-    let obj = [
-    ];
+    let obj = [];
     
     const play = () => {
         todoList.innerHTML = '';
@@ -18,13 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
             listElement.innerHTML = `<span>${item.value}</span>  <button class='deleteButton'>-</button>`;
             todoList.appendChild(listElement);
         });
+        let  deleteButton = document.querySelectorAll('.deleteButton');
+        deleteButton.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                deleteItem(obj, index);
+            })
+        })
     };
 
-    // play();
     if(localStorage.getItem('todo')){
         obj = JSON.parse(localStorage.getItem('todo'));
         play();
-    }
+    }else play();
+
     const addItem = () => {
         let tempObj = {
             value: '',
@@ -37,28 +39,17 @@ document.addEventListener('DOMContentLoaded', () => {
     todoButton.addEventListener('click', () => {
         if(todoInput.value){
             addItem();
-            // todoList.innerHTML = '';
             play();
             todoInput.value = '';
         }else alert('Вы не можете ввести пустое значение');
     });
 
-    
-    
-    const deleteButton = document.querySelectorAll('.deleteButton');
-    console.log(obj);
-
     const deleteItem = (object, index) => {
-        let tempArr = object.filter((item, itemIndex) => index != itemIndex)
+        deleteButton = document.querySelectorAll('.deleteButton');
+        let tempArr = object.filter((_item, itemIndex) => index != itemIndex)
         obj = tempArr;
         console.log(obj);
         localStorage.setItem('todo', JSON.stringify(obj));
         play();
     }
-
-    deleteButton.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            deleteItem(obj, index);
-        })
-    })
 });
